333644d7a759047615d1e5ca7133b77db00707f3
Nimmo-san <nat25oloman@gmail.com>
2025-06-22 13:25:28 +0100

    initial commit

---
+++ train_mnist.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+# 
+from torchvision import datasets, transforms # transforms the dataset
+from torch.utils.data import DataLoader # loads the dataset in batches
+
+
+
+# load the dataset
+
+
+# define dataloader for batch processing
+
+
+# also define a simple neural network model
+
+
+# training function
+
+
+# evaluating function
+
+
