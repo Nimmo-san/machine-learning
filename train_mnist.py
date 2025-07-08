e7ccba4ed3c567e243b0014a5368da4fda082207
Nimmo-san <nat25oloman@gmail.com>
2025-07-08 22:56:17 +0100

    change: major additions/refactoring

--- train_mnist.py
+++ train_mnist.py
@@ -3,30 +3,41 @@ import torch.nn as nn # neural network layers and loss functions
 import torch.optim as optim # optimisers such as SGD, Adam, etc
 
 from torchvision import datasets, transforms # datasets for common datasets, transforms for preprocessing functionality
-from torch.utils.data import DataLoader # efficiently loads and shuffles the dataset in batches
+from torch.utils.data import DataLoader  # efficiently loads and shuffles the dataset in batches
 # from torch.utils.data import random_split
+from pathlib import Path
+
+data_dir = "./data"
+model_save_path = Path("mnist_cnn.pt")
 
 
 # Defining the cnn model
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
+        super().__init__()
 
-        self.model = nn.Sequential(
-            nn.Conv2d(1, 16, 3, 1), # conv2d extracts patterns from 2d images 
-            nn.ReLU(),              # relu activation function that introduced non-linearty
-            nn.MaxPool2d(2),        # maxpool2d downsamples spatial dimensions (reduces size, increase abstraction)
-            nn.Conv2d(16, 32, 3, 1),
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 6, 3),
             nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Flatten(),           # flatten turns a 2d tensor into a 1d vector for feeding into dense layers
-            nn.Linear(800, 128),
+            nn.BatchNorm2d(6),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, 3),
             nn.ReLU(),
-            nn.Linear(128, 10) # 10 output classes for digits 0-9, fully connected layer for classification
+            nn.BatchNorm2d(16),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.out = nn.Sequential(
+            nn.Flatten(),
+            nn.LazyLinear(128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
+            nn.Softmax(dim=-1),
         )
 
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, X):
+        X = self.cnn(X)
+        X = self.out(X)
+        return X
 
 
 
@@ -36,48 +47,60 @@ transform = transforms.Compose([
     transforms.Normalize((.1307,), (.3081,)) # normalises the data mean and std from mnist to improve training stability
 ])
 
-# full MNIST training dataset
-# full_train_dataset = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
-
-# splitting 48,000 for training, 12,000 for validation
-# train_size = int(0.8 * len(full_train_dataset))
-# val_size = len(full_train_dataset) - train_size
-# train_dataset, val_dataset = random_split(full_train_dataset, [train_size, val_size])
-
-# dataLoaders after splitting data for batch processing
-# train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-# val_loader = DataLoader(val_dataset, batch_size=64)
-
 # without splitting data
 train_dataset = datasets.MNIST(
-    root="./data", train=True, transform=transform, download=True
+    root=data_dir, train=True, transform=transform, download=True
+)
+val_dataset = datasets.MNIST(
+    root=data_dir, train=False, transform=transform, download=True
 )
 
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True) # batches data and shuffles it every epoch
-
+val_loader = DataLoader(val_dataset, batch_size=1000, shuffle=False)
 
 # # model, loss, optimiser
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = CNN().to(device=device)
 loss_fn = nn.CrossEntropyLoss() # combines softmax and negative log-likelihood, good for classification
-optimiser = optim.Adam(model.parameters(), lr=0.001) # efficient optimiser with adaptive learning rates
+optimiser = optim.Adam(model.parameters(), lr=1e-3) # efficient optimiser with adaptive learning rates
 
+
+
+EPOCH = 50
+best_acc = 0.0
 
 # # training loop
-for epoch in range(1, 6):
+for epoch in range(1, EPOCH+1):
     model.train()
-    total_loss = 0
 
-    for X, y in train_loader:
-        X, y = X.to(device), y.to(device)
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
+    for data, target in train_loader:
+        data, target = data.to(device), target.to(device)
         optimiser.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
         loss.backward()
         optimiser.step()
 
-        total_loss += loss.item()
-    print(f"{epoch=}, Loss: {total_loss:.2f}")
+    # validation
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            pred = output.argmax(dim=1)
+            correct += pred.eq(target).sum().item()
+            total += target.size(0)
+    acc = correct / total
+    print(f"{epoch=}: {acc:.4f}")
 
-torch.save(model.state_dict(), "mnist_cnn.pt")
+    if acc > best_acc:
+        best_acc = acc
+    # if best_acc >= .98:
+    #     break
+
+print(f"{best_acc:.4f}")
+model.cpu()
+torch.save(model.state_dict(), model_save_path)
+print(f"Saved model to {model_save_path.resolve()}")
